6f74b48f907a9bffaba40184481ec326174c7a61
lokeshkharkwal7 <lokeshkharkwal@gmail.com>
2024-03-24 01:16:48 +0530

    III Commit done

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 // app.use(cors());
 
 app.use( cors({
-  origin: ["https://black-coffer-data-visualization-dashboard-fe-main.vercel.app"],
+  origin: ["https://data-analytical-dashbard.vercel.app"],
   // origin: ["https://wizstore.vercel.app"],
  
     methods: ["GET", "POST", "PUT", "DELETE"],
