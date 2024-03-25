71ef232650d4baa571cb7e09ee5fe7c18431712b
Lokesh Kharkwal <115694554+lokeshkharkwal7@users.noreply.github.com>
2024-03-25 17:23:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.use( cors({
   
-  origin: ["https://analytical-dashboard-six.vercel.app"],
+  origin: ["https://analytical-dashboard-kappa.vercel.app"],
   // origin: ["https://wizstore.vercel.app"],
  
     methods: ["GET", "POST", "PUT", "DELETE"],
