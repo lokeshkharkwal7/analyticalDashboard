df93afae626d7dd9d77f9a53815e528c2a6fec3e
Lokesh Kharkwal <115694554+lokeshkharkwal7@users.noreply.github.com>
2024-03-25 17:32:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,16 @@ let cors = require("cors");
 
 const app = express();
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
-app.use( cors({
+// app.use( cors({
   
-  origin: ["https://analytical-dashboard-kappa.vercel.app"],
-  // origin: ["https://wizstore.vercel.app"],
+//   origin: ["https://analytical-dashboard-kappa.vercel.app"],
+//   // origin: ["https://wizstore.vercel.app"],
  
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  // credentials: true
-}));
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   // credentials: true
+// }));
 
 // importing config files
 const mongoConnect = require("./connect");
